d8c38f7ed3cbd26779b628c405ab261bd99ce80b
Nicolas <nicolaskrusser@gmail.com>
2019-06-05 18:42:12 +0200

    added Higher-Order Components

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Wrap from '../Wrap/Wrap';
+import classes from './Layout.css';
+import Toolbar from '';
+
+class Layout extends Component {
+
+  render(){
+    return (
+      <Wrap>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+        />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+    </Wrap>
+    );
+  };
+};
+
+const mapStateToProps = state =>{
+    return {
+        isAuthenticated : state.auth.token !== null
+    }
+}
+export default connect(mapStateToProps)(Layout);
