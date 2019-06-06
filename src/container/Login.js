60e4d3bd497c233cdae0c562b10be3b99431d45f
Nicolas <nicolaskrusser@gmail.com>
2019-06-06 17:32:42 +0200

    added inputs and styles

--- src/container/Login.js
+++ src/container/Login.js
@@ -2,22 +2,32 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import errorHandler from '../hoc/withErrorHandler/withErrorHandler';
 import axios from 'axios';
+import classes from './Login.css';
 
 export class Login extends Component{
-    render(){
-        return(
-            <h1>Login</h1>
-        );
-    }
+  render(){
+    return(
+      <div className={classes.Login}>
+        <h1>Log in</h1>
+        <input placeholder="Email address" type="text"/>
+        <input placeholder="Password" type="password"/>
+        <div className={classes.Checkbox}>
+          <input type="checkbox"/>
+          <label htmlFor="scales">Keep me logged in</label>
+        </div>
+        <button>Login</button>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state =>{
-    return {
-        
-    };
+  return {
+      
+  };
 };
 const mapDispatchToProps = dispatch =>{
-    return{
-        
-    };
+  return{
+      
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(errorHandler(Login, axios));
